7e287a5e716fdb97dc5c0c2e10b1d9c625e40c64
Vadim <_s_e_t__@ukr.net>
2024-11-12 17:41:52 +0200

    added Quiz component

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,9 +1,42 @@
 import { useState } from "react";
 
+import QUESTIONS from "../questions";
+
+interface Question {
+  id: string;
+  text: string;
+  answers: string[];
+}
+
 const Quiz = () => {
-    const [activeQuestionIndex, setActiveQuestionIndex] = useState<number>(0);
+  const [userAnswers, setUserAnswers] = useState<string[]>([]);
+
+  const activeQuestionIndex = userAnswers.length;
+
+  const handleSelectAnswer = (selectedAnswer: string) => {
+    setUserAnswers((prevState) => [...prevState, selectedAnswer]);
+    };
     
-  return <p>Currently active Question</p>;
+    if (userAnswers.length === 7) {
+        return <p>You win</p>
+    }
+
+    return (
+      <div id="quiz">
+        <div id="question">
+          <p>{QUESTIONS[activeQuestionIndex].text}</p>
+          <ul id="answers">
+            {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+              <li key={answer} className="answer">
+                <button onClick={() => handleSelectAnswer(answer)}>
+                  {answer}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
 };
 
 export default Quiz;
